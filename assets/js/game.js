1944022f346a57269e625e910bfe5b96bb2c1e13
Thomas Walker <thomasnwalker@yahoo.com>
2021-05-09 10:24:13 -0700

    checking

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("this is an alert! JavaScript is running! ");
+function fight(){
+   window.alert("The fight has begun!");
+}
+
+
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log(" This logs a string, useful for leaving yourself "); 
+
+console.log( 10 + 10 );
+console.log("our robot's name is " + playerName); 
+
+var name = "your name";
+console.log(name);
+// fight();
